02c6be17d8fa1f53b0f6f4efcc84907e9be711d8
ana-07530 <bianeysita664@gmail.com>
2025-05-19 17:56:15 -0700

    agrege los archivos iniciales del proyecto de …

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import SearchBar from "./components/SearchBar"
+import ImageList from "./components/ImageList"
+import searchImages from "./api"
+
+function App(){
+    //searchImages('cars')
+    const [images, setImages] = useState([])
+
+    const handleSubmit = async(term) => {
+        console.log('Usted esta buscando con: ', term)
+        const result= await searchImages(term)
+        setImages(result)
+    }
+    return(
+        <>
+        <h1>Pictures App</h1>
+        <SearchBar onSubmit={handleSubmit}/>
+        <imagelist images={images}/>
+        </>
+    )
+}
+
+export default App
